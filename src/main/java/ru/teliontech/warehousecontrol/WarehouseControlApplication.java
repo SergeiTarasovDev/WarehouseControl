d710b8eb4c76c78d5c3f90b28ad476a06e66a479
Tarasov Sergei <www.neon@mail.ru>
2023-06-23 17:44:25 +0500

    initial commit

---
+++ src/main/java/ru/teliontech/warehousecontrol/WarehouseControlApplication.java
@@ -0,0 +1,13 @@
+package ru.teliontech.warehousecontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseControlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/teliontech/warehousecontrol/WarehouseControlApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.teliontech.warehousecontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
