efb27911556ab52f9660554f509bd79e664d3411
telion85 <www.neon@mail.ru>
2023-06-24 11:31:34 +0500

    created test for controller

--- src/test/java/ru/teliontech/warehousecontrol/controller/SockControllerTests.java
+++ src/test/java/ru/teliontech/warehousecontrol/controller/SockControllerTests.java
@@ -1,6 +1,8 @@
 package ru.teliontech.warehousecontrol.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,8 @@ import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import ru.teliontech.warehousecontrol.dto.SockDto;
+import ru.teliontech.warehousecontrol.dto.SockQntDto;
 import ru.teliontech.warehousecontrol.entity.Sock;
 import ru.teliontech.warehousecontrol.repository.SockRepository;
 import ru.teliontech.warehousecontrol.repository.TradingActionRepository;
@@ -18,8 +22,10 @@ import ru.teliontech.warehousecontrol.utils.MappingUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -60,29 +66,211 @@ public class SockControllerTests {
         mockMvc.perform(get("/api/socks/all")
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andReturn().getResponse();
+                .andExpect(status().isOk());
     }
 
     @Test
-    public void testGetCountSocksWithParams() throws Exception {
-        List<Sock> socks = new ArrayList<>(List.of(
-                new Sock(1L, "white", 50, 10),
-                new Sock(2L, "white", 50, 7)
-        ));
+    public void testGetCountSocksWithParams_WhiteMoreThan40() throws Exception {
+        String color = "white";
+        String operation = "moreThan";
+        int cottonPart = 40;
 
-        when(sockRepository.findAllByColorAndCottonPartGreaterThan("white", 40)).thenReturn(socks);
+        List<Sock> socks = new ArrayList<>(List.of(
+                new Sock(1L, color, 50, 10),
+                new Sock(2L, color, 20, 5),
+                new Sock(3L, color, 10, 3),
+                new Sock(4L, color, 50, 7)
+        ));
+        Integer expectedSum = socks.stream()
+                .filter(sock -> sock.getCottonPart() > cottonPart)
+                .mapToInt(Sock::getStock)
+                .sum();
+
+        when(sockRepository.getStockSumByColorAndCottonPartGreaterThan(color, cottonPart)).thenReturn(expectedSum);
         String response = mockMvc.perform(MockMvcRequestBuilders
                         .get("/api/socks")
-                        .queryParam("color", "white")
-                        .queryParam("operation", "moreThan")
-                        .queryParam("cottonPart", "40")
+                        .queryParam("color", color)
+                        .queryParam("operation", operation)
+                        .queryParam("cottonPart", String.valueOf(cottonPart))
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andReturn().getResponse().getContentAsString();
 
         Integer actual = new ObjectMapper().readValue(response, Integer.class);
-        assertEquals(17, actual);
+        assertEquals(expectedSum, actual);
     }
+
+    @Test
+    public void testGetCountSocksWithParams_WhiteLessThan40() throws Exception {
+        String color = "white";
+        String operation = "lessThan";
+        int cottonPart = 40;
+
+        List<Sock> socks = new ArrayList<>(List.of(
+                new Sock(1L, color, 50, 10),
+                new Sock(2L, color, 20, 5),
+                new Sock(3L, color, 10, 3),
+                new Sock(4L, color, 50, 7)
+        ));
+        Integer expectedSum = socks.stream()
+                .filter(sock -> sock.getCottonPart() < cottonPart)
+                .mapToInt(Sock::getStock)
+                .sum();
+
+        when(sockRepository.getStockSumByColorAndCottonPartLessThan(color, cottonPart)).thenReturn(expectedSum);
+
+        String response = mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/socks")
+                        .queryParam("color", color)
+                        .queryParam("operation", operation)
+                        .queryParam("cottonPart", String.valueOf(cottonPart))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+        Integer actual = new ObjectMapper().readValue(response, Integer.class);
+        assertEquals(expectedSum, actual);
+    }
+
+    @Test
+    public void testIncomeSock_Positive() throws Exception {
+        SockQntDto sockQntDto = new SockQntDto(1L, "white", 40, 11, 0);
+        int expectedStock = sockQntDto.getStock() + sockQntDto.getQuantity();
+        Sock sock = mappingUtils.mapToSock(sockQntDto);
+        List<Sock> socks = new ArrayList<>(List.of(
+                sock
+        ));
+
+        JSONObject sockObj = new JSONObject();
+        sockObj.put("id", sockQntDto.getId());
+        sockObj.put("color", sockQntDto.getColor());
+        sockObj.put("cottonPart", sockQntDto.getCottonPart());
+        sockObj.put("quantity", sockQntDto.getQuantity());
+        sockObj.put("stock", sockQntDto.getStock());
+
+        when(sockRepository.findByColorAndCottonPart(sockQntDto.getColor(), sockQntDto.getCottonPart())).thenReturn(socks);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/socks/income")
+                        .content(sockObj.toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(sockQntDto.getId()))
+                .andExpect(jsonPath("$.color").value(sockQntDto.getColor()))
+                .andExpect(jsonPath("$.cottonPart").value(sockQntDto.getCottonPart()))
+                .andExpect(jsonPath("$.quantity").value(sockQntDto.getQuantity()))
+                .andExpect(jsonPath("$.stock").value(expectedStock))
+        ;
+    }
+
+    @Test
+    public void testOutcomeSock_Positive() throws Exception {
+        SockQntDto sockQntDto = new SockQntDto(1L, "white", 40, 11, 20);
+        int expectedStock = sockQntDto.getStock() - sockQntDto.getQuantity();
+        Sock sock = mappingUtils.mapToSock(sockQntDto);
+        List<Sock> socks = new ArrayList<>(List.of(
+                sock
+        ));
+
+        JSONObject sockObj = new JSONObject();
+        sockObj.put("id", sockQntDto.getId());
+        sockObj.put("color", sockQntDto.getColor());
+        sockObj.put("cottonPart", sockQntDto.getCottonPart());
+        sockObj.put("quantity", sockQntDto.getQuantity());
+        sockObj.put("stock", sockQntDto.getStock());
+
+        when(sockRepository.findByColorAndCottonPart(sockQntDto.getColor(), sockQntDto.getCottonPart())).thenReturn(socks);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/socks/outcome")
+                        .content(sockObj.toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(sockQntDto.getId()))
+                .andExpect(jsonPath("$.color").value(sockQntDto.getColor()))
+                .andExpect(jsonPath("$.cottonPart").value(sockQntDto.getCottonPart()))
+                .andExpect(jsonPath("$.quantity").value(sockQntDto.getQuantity()))
+                .andExpect(jsonPath("$.stock").value(expectedStock))
+        ;
+    }
+
+    @Test
+    public void testCreateSock_Positive() throws Exception {
+        SockDto sockDto = new SockDto(1L, "white", 40, 20);
+        JSONObject sockObj = getJsonObject(sockDto);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/socks")
+                        .content(sockObj.toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(sockDto.getId()))
+                .andExpect(jsonPath("$.color").value(sockDto.getColor()))
+                .andExpect(jsonPath("$.cottonPart").value(sockDto.getCottonPart()))
+                .andExpect(jsonPath("$.stock").value(sockDto.getStock()))
+        ;
+    }
+
+    @Test
+    public void testUpdateSock_Positive() throws Exception {
+        SockDto sockDto = new SockDto(1L, "white", 40, 20);
+
+        JSONObject sockObj = new JSONObject();
+        sockObj.put("id", sockDto.getId());
+        sockObj.put("color", sockDto.getColor());
+        sockObj.put("cottonPart", sockDto.getCottonPart());
+        sockObj.put("stock", sockDto.getStock());
+
+        Optional<Sock> sock = Optional.of(mappingUtils.mapToSock(sockDto));
+        when(sockRepository.findById(sockDto.getId())).thenReturn(sock);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .patch("/api/socks")
+                        .content(sockObj.toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(sockDto.getId()))
+                .andExpect(jsonPath("$.color").value(sockDto.getColor()))
+                .andExpect(jsonPath("$.cottonPart").value(sockDto.getCottonPart()))
+                .andExpect(jsonPath("$.stock").value(sockDto.getStock()))
+        ;
+    }
+
+    @Test
+    public void testDeleteSock_Positive() throws Exception {
+        String color = "white";
+        List<Sock> expectedSocks = new ArrayList<>(List.of(
+                new Sock(1L, color, 50, 10),
+                new Sock(2L, color, 20, 5),
+                new Sock(3L, color, 10, 3),
+                new Sock(4L, color, 50, 7)
+        ));
+        SockDto sockDto = mappingUtils.mapToSockDto(expectedSocks.get(0));
+        Sock sock = mappingUtils.mapToSock(sockDto);
+        Optional<Sock> optionalSock = Optional.of(sock);
+
+        when(sockRepository.findById(any(Long.class))).thenReturn(optionalSock);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .delete("/api/socks/3")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+        ;
+    }
+
+    private JSONObject getJsonObject(SockDto sockDto) throws JSONException {
+        JSONObject sockObj = new JSONObject();
+        sockObj.put("id", sockDto.getId());
+        sockObj.put("color", sockDto.getColor());
+        sockObj.put("cottonPart", sockDto.getCottonPart());
+        sockObj.put("stock", sockDto.getStock());
+        return sockObj;
+    }
+
 }
