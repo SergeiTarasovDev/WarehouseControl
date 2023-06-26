808c72ef698a77989a82754dcd8a30102743407f
Tarasov Sergei <www.neon@mail.ru>
2023-06-26 13:27:18 +0500

    added api description

--- src/main/java/ru/teliontech/warehousecontrol/controller/SockController.java
+++ src/main/java/ru/teliontech/warehousecontrol/controller/SockController.java
@@ -1,7 +1,12 @@
 package ru.teliontech.warehousecontrol.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.teliontech.warehousecontrol.dto.SockDto;
@@ -23,11 +28,44 @@ public class SockController {
         this.sockService = sockService;
     }
 
+    @Operation(
+            tags = "Учет носков на складе",
+            summary = "Получить список товаров",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "OK",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = List.class)
+                            )
+                    )
+            }
+    )
     @GetMapping("/all")
     public ResponseEntity<List<SockDto>> getAllSocks() {
         return ResponseEntity.ok(sockService.getAllSocks());
     }
 
+    @Operation(
+            tags = "Учет носков на складе",
+            summary = "Возвращает общее количество носков на складе, соответствующих переданным в параметрах критериям запроса.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "OK",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = Integer.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Bad request",
+                            content = @Content()
+                    )
+            }
+    )
     @GetMapping()
     public ResponseEntity<Integer> getCountSocksWithParams(
             @RequestParam String color,
@@ -36,26 +74,121 @@ public class SockController {
         return ResponseEntity.ok(sockService.getCountSocksWithParams(color, operation, cottonPart).orElseThrow(() -> new InvalidResultException(EXCMSG_UNEXPECTED_VALUE)));
     }
 
+    @Operation(
+            tags = "Учет носков на складе",
+            summary = "Регистрирует приход носков на склад.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "OK",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = SockQntDto.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Bad request",
+                            content = @Content()
+                    )
+            }
+    )
     @PostMapping("/income")
     public ResponseEntity<SockQntDto> incomeSock(@RequestBody SockQntDto sockQntDto) {
         return ResponseEntity.ok(sockService.income(sockQntDto).orElseThrow(() -> new InvalidResultException(EXCMSG_UNEXPECTED_VALUE)));
     }
 
+    @Operation(
+            tags = "Учет носков на складе",
+            summary = "Регистрирует отпуск носков со склада.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "OK",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = SockQntDto.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Bad request",
+                            content = @Content()
+                    )
+            }
+    )
     @PostMapping("/outcome")
     public ResponseEntity<SockQntDto> outcomeSock(@RequestBody SockQntDto sockQntDto) {
         return ResponseEntity.ok(sockService.outcome(sockQntDto).orElseThrow(() -> new InvalidResultException(EXCMSG_UNEXPECTED_VALUE)));
     }
 
+    @Operation(
+            tags = "Учет носков на складе",
+            summary = "Добавление нового вида носков в базу данных",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "OK",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = SockDto.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Bad request",
+                            content = @Content()
+                    )
+            }
+    )
     @PostMapping()
     public ResponseEntity<SockDto> createSock(@RequestBody SockDto sockDto) {
         return ResponseEntity.ok(sockService.createSock(sockDto).orElseThrow(() -> new InvalidResultException(EXCMSG_UNEXPECTED_VALUE)));
     }
 
+    @Operation(
+            tags = "Учет носков на складе",
+            summary = "Обновление данных в базе данных",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "OK",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = SockDto.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Bad request",
+                            content = @Content()
+                    )
+            }
+    )
     @PatchMapping()
     public ResponseEntity<SockDto> updateSock(@RequestBody SockDto sockDto) {
         return ResponseEntity.ok(sockService.updateSock(sockDto).orElseThrow(() -> new InvalidResultException(EXCMSG_UNEXPECTED_VALUE)));
     }
 
+    @Operation(
+            tags = "Учет носков на складе",
+            summary = "Удаление записи из базы данных",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "OK",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = SockDto.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Bad request",
+                            content = @Content()
+                    )
+            }
+    )
     @DeleteMapping("/{id}")
     public ResponseEntity<SockDto> deleteSock(@PathVariable Long id) {
         return ResponseEntity.ok(sockService.deleteSock(id).orElseThrow(() -> new InvalidResultException(EXCMSG_UNEXPECTED_VALUE)));
